1a4e4b86ad0e0bfb96d1494687a167bd08f26a03
etashsingh <etashsingh29@gmail.com>
2019-11-25 10:39:19 +0530

    ok

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,15 +8,18 @@ public class App
 {
 
     private final String message = "Hello World!";
+    private final String message2 = "OKAY OKAY";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage2());
     }
 
     private final String getMessage() {
         return message;
     }
+    private final String getMessage2() { return message2; }
 
 }
